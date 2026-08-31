05266e55184ea20568811f55b2541e18e3c1c6c4
lcolosi <lcolosi@ucsd.edu>
2026-08-31 12:10:16 -0700

    Rename fig04.py to fig06.py and fig09.py to fig04.py

--- src/fig04.py
+++ src/fig04.py
@@ -1,22 +1,19 @@
 # =============================================================================
-# Figure 04
+# Figure 09
 # =============================================================================
 #
 # Caption:
-#   Decorrelation time scale of potential density (solid curve) as a function of
-#   depth from model data at (a) CCE1, (b) CCE2, and (c) CCE3, and observations
-#   at (d) CCE1 and (e) CCE2. The standard error of the mean for the decorrelation 
-#   scale is shown as the shaded regions. The seasonally averaged mixed-layer depth
-#   $\overline{z}_{mld}$ and its standard deviation are displayed as horizontal
-#   dashed lines and light shaded regions, respectively. For CCE1, CCE2, and CCE3
-#   model data, solid black circles denote the vertical model grid. For CCE1 and CCE2
-#   observations, solid back diamonds denote the depth of the CTD sensor.
+#   Decorrelation time scale in the study domain at 9 meter water depth. Black
+#   contour lines are the ocean topography with 200 and 2000 meter isobaths 
+#   highlighted as solid black lines. Decorrelation scales less than or equal to 
+#   one standard error are considered not statistically significant and are indicated
+#   with a hatched overlay.
 #
 # Author:
 #   Luke Colosi
 #
 # Created:
-#   2026-08-19
+#   2026-08-18
 # =============================================================================
 
 # Import libraries 
@@ -25,6 +22,9 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt 
 from netCDF4 import Dataset
+import cartopy.crs as ccrs
+import cmocean.cm as cmo
+import matplotlib as mpl
 
 # Set path to project root directory
 ROOT = Path(__file__).resolve().parents[1]
@@ -37,8 +37,8 @@ PATH_tools = ROOT / "tools"
 # Set path to access additional python functions
 sys.path.append(str(PATH_tools))
 
-# Import plotting toolbox 
-from plotting import add_corner_label
+# Import plotting toolbox for cartopy figures
+from plotting import set_coastlines, set_grid_ticks, set_cbar, add_scalebar
 
 # -----------------------------------------------------------------------------
 # Set processing and plotting parameters
@@ -50,17 +50,33 @@ from plotting import add_corner_label
 #
 # - option_data: Data variable to analyze.
 #                Options: "temp", "sal", "density", "uvel", "vvel", or "ssh".
+#
+# - option_depth: Depth at which the decorrelation time scale is computed
+#                 (units: meters). Not used for SSH.
 # - option_interannual: Specifies the model of the interannual variability. 
 #                       Options include: 'linear' or 'gaussian'
+# - option_detrend_seg: Specifies whether each segment is detrended or not. 
+#                        Options: True or False
+# - segment_months : Specifies the window duration. 
+# - ns : Noise to signal ratio. 
 #
 # ------------#
 
 # Set processing parameters
 option_data        = 'density'    
+option_depth       = 9   
 option_interannual = 'linear' 
+option_detrend_seg = True
+segment_months     = 8
+
+# Label segment processing 
+seg_proc = "detrend" if option_detrend_seg else "demean"
+
+# Set uncertainty estimate parameters
+ns = 1
 
 # Set font and fontsize using LaTeX 
-fontsize=16
+fontsize=18
 plt.rcParams.update({
     "font.size": fontsize,         
     "text.usetex": True,           
@@ -72,274 +88,285 @@ plt.rcParams.update({
 # Load MITgcm decorrelation scales, bathymetry, CCE, and CalCOFI data
 # -----------------------------------------------------------------------------
 
-# --- MITgcm Data --- # 
+# --- Decorrelation Time Scales --- # 
 
 # Set path to processed regional MITgcm data
-PATH_processed = PATH_data / "mitgcm" / "mooring" / "processed"
+PATH_processed = PATH_data / "mitgcm" / "regional" / "processed"
 
 # Obtain filename paths
-filename_mitgcm = PATH_processed / f"mitgcm_decor_scale_{option_data}_hrly_mooring_{option_interannual}.nc"
+filename_mitgcm = PATH_processed / f"mitgcm_decor_scale_{option_data}_hrly_reg_depth_{option_depth}m_{option_interannual}_{seg_proc}_seg_duration_{segment_months}mo.nc"
 
 # Generate the nc data structure
 nc = Dataset(filename_mitgcm, 'r')
 
 # Extract data variables
-site    = nc.variables['site'][:]
-depth_m = nc.variables['depth'][:]
+lon     = nc.variables['lon'][:]
+lat     = nc.variables['lat'][:]
 Lt      = nc.variables['decor_scale'][:]
 Lt_stdm = nc.variables['decor_scale_stdm'][:]
 Lt_std  = nc.variables['decor_scale_std'][:]
 
-# --- CCE Data --- # 
+# --- Bathymetry --- # 
 
+# Obtain filename path
+filename_bathy = PATH_data / "bathymetry" / "etopo1_point_conception.nc"
+
+# Generate the nc data structure
+nc_bathy = Dataset(filename_bathy, 'r')
+
+# Extract data variables
+lon_b  = nc_bathy.variables['lon'][:]
+lat_b  = nc_bathy.variables['lat'][:]
+bathy  = nc_bathy.variables['BATHY'][:]
+
+# --- CCE Mooring Locations --- # 
+lat1, lat2, lat3  = 33.457, 34.3075, 34.44825228022894           
+lon1, lon2, lon3  = -122.52233, -120.8042, -120.53825701527784 
+
+# --- CalCOFI Line 80.0 Positions --- # 
+
+# Obtain filename path
+filename = PATH_data / "calcofi" / "CalCOFIStationOrder.csv"
+
+# Load csv file 
+calCOFI_data = np.genfromtxt(
+    filename,
+    delimiter=",",
+    skip_header=1,
+    usecols=(1, 3, 7, 11),
+    invalid_raise=False
+)
+
+# Grab stations on line 80.0
+calCOFI_line80 = calCOFI_data[calCOFI_data[:, 0] == 80.0] 
+
+# Parse data into separate arrays
+calCOFI_lat   = calCOFI_line80[:, 1]
+calCOFI_lon   = calCOFI_line80[:, 2]
 
 # -----------------------------------------------------------------------------
-# Plot decorrelation time scales at mooring locations 
+# Compute the relative uncertainty of the decorrelation scale
 # -----------------------------------------------------------------------------
 
-# Set plotting parameters 
-depth_pos_m = abs(depth_m)
-depth_lim = [0,200]
-cce1_sensor_depth = np.array([9, 19, 29, 39, 60, 75, 150])
-cce2_sensor_depth = np.array([6, 14, 25, 44, 74])
+# Compute spatial mean
+Lt_reg_mean = np.ma.mean(Lt)
+
+# Compute the relative uncertainty (with respect to the regional mean)
+Lt_rel_unc = Lt_stdm / np.abs(Lt - Lt_reg_mean)
+
+# Mask not statistically significant grid points
+Lt_mask = np.ma.getmask(np.ma.masked_greater_equal(Lt_rel_unc, ns))
+
+# Get land mask from Lt
+land_mask = np.ma.getmaskarray(Lt)
+
+# Combine statistical significance and land masks
+Lt_mask = Lt_mask & ~land_mask
+
+# Create a mask array where non-significant ocean points = 1, others = NaN
+data_mask = np.where(Lt_mask, 1, np.nan)
+
+# -----------------------------------------------------------------------------
+# Plot regional decorrelation time scales  
+# -----------------------------------------------------------------------------
+
+# Set plotting parameters
+projection = ccrs.PlateCarree(central_longitude=0.0)
+xticks = [-123, -122, -121, -120]
+yticks = [33.25, 33.50, 33.75, 34.00, 34.25, 34.50, 34.75, 35.00]
+resolution = "10m"
+bounds = np.arange(0,360+40,40)
+lon_min, lon_max = -123, -120
+lat_min, lat_max = 33, 35
+levels = np.arange(8,32+0.5,0.5) 
+level_is = np.arange(100,300,100)
+levels_ms = np.arange(1000,3000,500)
+fontsize_g = 18
+fontsize_c = 10
+cmap = cmo.amp
+mpl.rcParams["hatch.linewidth"] = 0.2 
 
 # Create figure
-fig, axes = plt.subplots(2,3,figsize=(15, 10))
-ax_flat = axes.flatten()
+fig, ax = plt.subplots(figsize=(12, 8), subplot_kw={"projection": projection})
 
-# --- Subplot 1 --- # 
-ax = ax_flat[0]
+# Plot coastlines and land 
+set_coastlines(
+    ax, 
+    projection, 
+    resolution, 
+    lon_min=lon_min, 
+    lon_max=lon_max, 
+    lat_min=lat_min, 
+    lat_max=lat_max
+) 
 
-# Plot CCE1 potential density decor scale
-ax.plot(Lt[0,:],depth_pos_m,'.-', color='tab:green', label='CCE1')
-
-# Plot standard error of the mean
-ax.fill_betweenx(depth_pos_m, Lt[0,:] - Lt_stdm[0,:], Lt[0,:] + Lt_stdm[0,:], color='tab:green', alpha=0.5)
-
-# Set left edge x-position
-x_right = ax.get_xlim()[0] - 13.6  
-
-# Plot model grid depth levels
-ax.plot(
-    np.full_like(depth_pos_m[:-1], x_right),
-    depth_pos_m[:-1],
-    marker='.', 
-    linestyle='None',
-    color='k', 
-    markersize=6, 
-    alpha=0.6,
-    clip_on=False,
+# Plot decorrelation time scales
+ct = ax.contourf(
+    lon, 
+    lat, 
+    Lt, 
+    levels=levels,
+    transform=ccrs.PlateCarree(),
+    cmap=cmap, 
+    extend='both'
 )
 
-# Set axis attributes
-ax.set_ylabel('Depth (m)')
-ax.set_xlim(0,45)
-ax.set_ylim(depth_lim[0], depth_lim[1])
-ax.set_xticks(np.arange(0,45+5,5))
-ax.set_yticks(np.arange(0,200+25,25))
-ax.set_xticklabels([])
-ax.invert_yaxis()
-ax.tick_params(top=True, 
-               bottom=True, 
-               left=True, 
-               right=True, 
-               labelleft=True,
-               direction='out', 
-               length=3.5)
-ax.grid(True,linestyle='--',alpha=0.3)
-
-# --- Subplot 2 --- # 
-ax = ax_flat[1]
-
-# Plot CCE1 potential density decor scale
-ax.plot(Lt[1,:],depth_pos_m,'.-', color='tab:red', label='CCE1')
-
-# Plot standard error of the mean
-ax.fill_betweenx(depth_pos_m, Lt[1,:] - Lt_stdm[1,:], Lt[1,:] + Lt_stdm[1,:], color='tab:red', alpha=0.5)
-
-# Set left edge x-position
-x_left = ax.get_xlim()[0] - 10.4  
-
-# Plot model grid depth levels
-ax.plot(
-    np.full_like(depth_pos_m[:-1], x_left),
-    depth_pos_m[:-1],
-    marker='.', 
-    linestyle='None',
-    color='k', 
-    markersize=6, 
-    alpha=0.6,
-    clip_on=False,
+# Overlay a contourf with hatching for the non-significant regions
+ax.contourf(
+    lon,
+    lat,
+    data_mask,
+    levels=[0.5, 1.5],      
+    hatches=['..'],        
+    colors='none',          
+    zorder=10,              
+    transform=ccrs.PlateCarree()
 )
 
-# Set axis attributes
-ax.set_xlim(0,45)
-ax.set_ylim(depth_lim[0], depth_lim[1])
-ax.set_xticks(np.arange(0,45+5,5))
-ax.set_yticks(np.arange(0,200+25,25))
-ax.set_xticklabels([])
-ax.set_yticklabels([])
-ax.invert_yaxis()
-ax.tick_params(top=True, 
-               bottom=True, 
-               left=True, 
-               right=True, 
-               labelleft=True,
-               direction='out', 
-               length=3.5)
-ax.grid(True,linestyle='--',alpha=0.3)
-
-# --- Subplot 3 --- # 
-ax = ax_flat[2]
-
-# Plot CCE1 potential density decor scale
-ax.plot(Lt[2,:],depth_pos_m,'.-', color='tab:blue', label='CCE3')
-
-# Plot standard error of the mean
-ax.fill_betweenx(depth_pos_m, Lt[2,:] - Lt_stdm[2,:], Lt[2,:] + Lt_stdm[2,:], color='tab:blue', alpha=0.5)
-
-# Set left edge x-position
-x_left = ax.get_xlim()[0] - 9 
-
-# Plot model grid depth levels
-ax.plot(
-    np.full_like(depth_pos_m[:-1], x_left),
-    depth_pos_m[:-1],
-    marker='.', 
-    linestyle='None',
-    color='k', 
-    markersize=6, 
-    alpha=0.6,
-    clip_on=False, 
-    label='Model grid',
+# Plot the CCE1 mooring point
+ax.scatter(
+    lon1, 
+    lat1, 
+    color='w',
+    edgecolor='black', 
+    marker='^', 
+    s=40, 
+    transform=ccrs.PlateCarree(),
+    zorder=10, 
+    label='CCE1'
 )
 
-# Set axis attributes
-ax.set_xlabel(r'Decorrelation Scale (days)')
-ax.set_xlim(0,45)
-ax.set_ylim(depth_lim[0], depth_lim[1])
-ax.set_xticks(np.arange(0,45+5,5))
-ax.set_yticks(np.arange(0,200+25,25))
-ax.set_yticklabels([])
-ax.invert_yaxis()
-ax.tick_params(top=True, 
-               bottom=True, 
-               left=True, 
-               right=True, 
-               labelleft=True,
-               direction='out', 
-               length=3.5)
-ax.grid(True,linestyle='--',alpha=0.3)
-
-#--- Subplot 4 ---# 
-ax = ax_flat[3]
-
-# Set left edge x-position 
-x_left = ax.get_xlim()[0]
-
-# Plot the sensor depths 
-ax.plot(
-    np.full_like(cce1_sensor_depth, x_left),
-    cce1_sensor_depth,
-    marker='d', 
-    linestyle='None',
-    color='k', 
-    markersize=5, 
-    alpha = 1, 
-    clip_on=False,
-    label='Sensor depth'  
+# Plot the CCE2 mooring point
+ax.scatter(
+    lon2, 
+    lat2, 
+    color='w',  
+    edgecolor='black', 
+    marker='s', 
+    s=40,  
+    transform=ccrs.PlateCarree(),
+    zorder=10, 
+    label='CCE2'
 )
 
-# Set axis attributes
-ax.set_ylabel('Depth (m)')
-ax.set_xlabel(r'Decorrelation Scale (days)')
-ax.set_xlim(0,45)
-ax.set_ylim(depth_lim[0], depth_lim[1])
-ax.set_xticks(np.arange(0,45+5,5))
-ax.set_yticks(np.arange(0,200+25,25))
-ax.invert_yaxis()
-ax.tick_params(top=True, 
-               bottom=True, 
-               left=True, 
-               right=True, 
-               labelleft=True,
-               direction='out', 
-               length=3.5)
-ax.grid(True,linestyle='--',alpha=0.3)
-
-#--- Subplot 5 ---# 
-ax = ax_flat[4]
-
-# Set left edge x-position 
-x_left = ax.get_xlim()[0] 
-
-# Plot the sensor depths 
-ax.plot(
-    np.full_like(cce2_sensor_depth, x_left),
-    cce2_sensor_depth,
-    marker='d', 
-    linestyle='None',
-    color='k', 
-    markersize=5, 
-    alpha = 1, 
-    clip_on=False,
+# Plot the CCE3 mooring point
+ax.scatter(
+    lon3, 
+    lat3, 
+    color= 'w',  
+    edgecolor='black', 
+    marker='o', 
+    s=40,  
+    transform=ccrs.PlateCarree(),
+    zorder=10, 
+    label='CCE3'
 )
 
-# Set axis attributes
-ax.set_xlabel(r'Decorrelation Scale (days)')
-ax.set_xlim(0,45)
-ax.set_ylim(depth_lim[0], depth_lim[1])
-ax.set_xticks(np.arange(0,45+5,5))
-ax.set_yticks(np.arange(0,200+25,25))
-ax.set_yticklabels([])
-ax.invert_yaxis()
-ax.tick_params(top=True, 
-               bottom=True, 
-               left=True, 
-               right=True, 
-               labelleft=True,
-               direction='out', 
-               length=3.5)
-ax.grid(True,linestyle='--',alpha=0.3)
+# Plot depth contour lines
+ct1 = ax.contour(
+    lon_b, 
+    lat_b, 
+    -1*(bathy),
+    levels=levels_ms, 
+    colors='black', 
+    linewidths=0.5, 
+    linestyles='dashed'
+)
+ct2 = ax.contour(
+    lon_b, 
+    lat_b, 
+    -1*(bathy),
+    levels=[2000], 
+    colors='black', 
+    linewidths=1, 
+    linestyles='solid'
+)
+ct3 = ax.contour(
+    lon_b, 
+    lat_b, 
+    -1*(bathy),
+    levels=level_is, 
+    colors='black', 
+    linewidths=0.5, 
+    linestyles='dashed'
+)
+ct4 = ax.contour(
+    lon_b, 
+    lat_b, 
+    -1*(bathy),
+    levels=[200], 
+    colors='black', 
+    linewidths=1, 
+    linestyles='solid'
+)
+plt.clabel(ct1, fontsize=fontsize_c)
+plt.clabel(ct2, fontsize=fontsize_c)
+plt.clabel(ct3, fontsize=fontsize_c)
+plt.clabel(ct4, fontsize=fontsize_c)
 
-#--- Subplot 6 ---# 
-ax = ax_flat[5]
+# Plot Line 80 CalCOFI Stations
+ax.plot(
+    calCOFI_lon % 360, 
+    calCOFI_lat,
+    color='k',
+    linestyle=(0, (5, 3)),  
+    linewidth=1.5,
+    transform=ccrs.PlateCarree(),
+)
 
-# Turn off axis
-ax.axis('off')
+# Set grid ticks 
+set_grid_ticks(
+    ax,
+    xticks=xticks,
+    yticks=yticks,
+    xlabels=True,
+    ylabels=True,
+    grid=True,
+    fontsize=fontsize_g,
+    color='k',
+    lw=1,
+    ls='--',
+    alpha=0.1
+)
 
-# Obtain the handle for the legend 
-handles = []
-labels = []
+# Create colormap
+cax = plt.axes([0.91, 0.3, 0.02, 0.4])
+set_cbar(
+    ct,
+    cax,
+    fig,
+    orientation="vertical",
+    extend="both",
+    label='Decorrelation Scale (days)',
+    fontsize=fontsize_g,
+    ticks=np.arange(8,32+4,4), 
+    invert = False
+)
 
-# Loop through axes that contain legend items
-for i in [0,1,2,3]:  
-    h, l = ax_flat[i].get_legend_handles_labels()
-    handles.extend(h)
-    labels.extend(l)
+# Set legend
+ax.legend(
+    loc='upper right',
+    fontsize=14,
+    framealpha=0.9,
+    edgecolor='black'
+)
 
-# Display legend in the position of the 6th axis
-ax.legend(handles, labels, loc='center', fontsize=16)
-
-# Label each subplot
-pos = [0.94, 0.07]
-add_corner_label(ax_flat[0], pos, 'A', fontsize = 16)
-add_corner_label(ax_flat[1], pos, 'B', fontsize = 16)
-add_corner_label(ax_flat[2], pos, 'C', fontsize = 16)
-add_corner_label(ax_flat[3], pos, 'D', fontsize = 16)
-add_corner_label(ax_flat[4], pos, 'E', fontsize = 16)
-
-# Adjust figure spacing
-plt.subplots_adjust(hspace=0.1, wspace=0.1)
+# Add a 20-km scale bar
+add_scalebar(
+    ax, 
+    length_km=20, 
+    location=(0.925, 0.78),
+    linewidth=1, 
+    text_kwargs=dict(fontsize=14, color='white', weight='bold')
+)
 
 # Save figure in high resolution 
 fig.savefig(
-    PATH_figs / "fig04.png",
+    PATH_figs / "fig09.png",
     dpi=300,
     facecolor='white',
     bbox_inches='tight',
     pad_inches=0.1,
     transparent=False
 )
-
 
